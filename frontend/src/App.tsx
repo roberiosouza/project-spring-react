2aca1b5f72bc3bf12a24ffc33fde639a6993b5bd
Robério Souza <rroberiosouza@gmail.com>
2021-09-12 23:47:20 -0300

    Basic statics components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from 'Components/DataTable';
+import Footer from 'Components/Footer';
+import NavBar from 'Components/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello World!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1>Corpo</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/Components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/roberiosouza/" target="_blank" rel="noreferrer">Roberio Souza</a></p>
+                <p className="text-light"><small><strong>Java + React = S2</strong><br />
+                </small></p>
+            </div>
+        </footer>
+    );
+
+}
+
+export default Footer;
