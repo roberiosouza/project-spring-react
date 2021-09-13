2095da37bded869df38ed5bf439ac1233850eb76
Robério Souza <rroberiosouza@gmail.com>
2021-09-13 19:28:25 -0300

    Ajuste no nome do Dev criador da aplicação

--- frontend/src/Components/Footer/index.tsx
+++ frontend/src/Components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/roberiosouza/" target="_blank" rel="noreferrer">Roberio Souza</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/roberiosouza/" target="_blank" rel="noreferrer">Robério Souza</a></p>
                 <p className="text-light"><small><strong>Java + React = S2</strong><br />
                 </small></p>
             </div>
